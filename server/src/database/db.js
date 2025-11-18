bdd4b9d8650beb5c4ce5b0715b3d8c0300e2d4f3
Zuzanna Kurpik <zuziakurp@gmail.com>
2025-11-18 19:37:05 +0100

    conection test with db by railway

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -2,11 +2,11 @@ import mysql from "mysql";
 import util from "util";
 
 const pool = mysql.createPool({
-  host: "34.57.186.71",
-  port: 3306,
-  user: "root",
-  password: "m>P?_9C[H*|H>8|l",
-  database: "study",
+  host: process.env.DB_HOST || "34.57.186.71",
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "m>P?_9C[H*|H>8|l",
+  database: process.env.DB_NAME || "study",
   ssl: {
     rejectUnauthorized: false
   },
