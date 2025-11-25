f593cf4661975b9593b24f4e56206fa735c5a035
Natalia Lazecka <natalia.lazecka@gmail.com>
2025-11-25 18:34:15 +0100

    update and delete tasks

--- server/src/routes/tasks.routes.js
+++ server/src/routes/tasks.routes.js
@@ -6,6 +6,6 @@ router.get("/", getTasks);
 router.post("/", addTask);
 router.put("/:id", updateTask);
 router.delete("/:id", deleteTask);
-router.get("/tasks/:id", getTaskById);
+router.get("/:id", getTaskById);
 
 export default router;
