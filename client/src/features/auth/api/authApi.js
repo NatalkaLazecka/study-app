81d3404a7aaf48c37570c56b820e082a29bc48c5
Natalia Lazecka <natalia.lazecka@gmail.com>
2025-12-10 19:44:57 +0100

    console logi debug

--- client/src/features/auth/api/authApi.js
+++ client/src/features/auth/api/authApi.js
@@ -2,43 +2,44 @@ const API = import.meta.env.VITE_API_URL;
 import axios from "axios";
 
 export async function sendResetEmail(email) {
-  return axios.post(`${API}/api/emails/reset`, { email });
+    console.log("Wysyłam reset na:", `${API}/api/emails/reset`);
+    console.log("Email:", email);
+    return axios.post(`${API}/api/emails/reset`, {email});
 }
 
 export async function verifyToken(token) {
-  const res = await axios.get(`${API}/api/emails/verify/${token}`);
-  return res.data;
+    const res = await axios.get(`${API}/api/emails/verify/${token}`);
+    return res.data;
 }
 
-export async function resetPassword({ token, newPassword }) {
-  return axios.post(`${API}/api/emails/reset-password`, {
-    token,
-    newPassword
-  });
+export async function resetPassword({token, newPassword}) {
+    return axios.post(`${API}/api/emails/reset-password`, {
+        token,
+        newPassword
+    });
 }
-
 
 
 export async function login(email, password) {
-  const res = await fetch(`${API}/api/auth/login`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password })
-  });
+    const res = await fetch(`${API}/api/auth/login`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email, password})
+    });
 
-  if (!res.ok) throw new Error("Login failed");
+    if (!res.ok) throw new Error("Login failed");
 
-  return res.json();
+    return res.json();
 }
 
 export async function register(data) {
-  const res = await fetch(`${API}/api/auth/register`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
-  });
+    const res = await fetch(`${API}/api/auth/register`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
+    });
 
-  if (!res.ok) throw new Error("Register failed");
+    if (!res.ok) throw new Error("Register failed");
 
-  return res.json();
+    return res.json();
 }
