a4ad9424a33f319fbbee77c3cc1047f8743ab6ad
Natalia Lazecka <natalia.lazecka@gmail.com>
2025-12-12 16:46:07 +0100

    test

--- server/src/services/email.service.js
+++ server/src/services/email.service.js
@@ -1,35 +1,42 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { Resend } from 'resend'
-import fs from 'fs'
-import path from 'path'
-import handlebars from 'handlebars'
-import { fileURLToPath } from 'url'
-import { RESEND_API_KEY, FRONTEND_URL } from '../config/env.js'
+import { Resend } from 'resend';
+import fs from 'fs';
+import path from 'path';
+import handlebars from 'handlebars';
+import { fileURLToPath } from 'url';
+import { FRONTEND_URL } from '../config/env.js';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 // konfiguracja ścieżki szablonu
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const templatePath = path.join(__dirname, '..', 'templates', 'reset-password.hbs')
-const templateSource = fs.readFileSync(templatePath, 'utf8')
-const tpl = handlebars.compile(templateSource)
+const templatePath = path.join(__dirname, '..', 'templates', 'reset-password.hbs');
+const templateSource = fs.readFileSync(templatePath, 'utf8');
+const tpl = handlebars.compile(templateSource);
 
-// wysyłanie e-maila resetującego hasło
 export async function sendResetEmail(email, token) {
-  const resetLink = `${FRONTEND_URL}/reset-password?token=${token}`
-  const html = tpl({ resetLink })
-try{
-     return resend.emails.send({
-    from: 'Study <noreply@resend.dev>',
-    to: email,
-    subject: 'Reset hasła',
-    html
-  });
-}catch (err){
-    console.log("resend err:", err);
-}
+  const resetLink = `${FRONTEND_URL}/reset-password?token=${token}`;
+  const html = tpl({ resetLink });
+
+  try {
+    console.log("Sending email to:", email);
+
+    const res = await resend.emails.send({
+      from: 'Study <noreply@resend.dev>',
+      to: email,
+      subject: 'Reset hasła',
+      html,
+    });
+
+    console.log("RESEND SUCCESS:", res);
+    return res;
+
+  } catch (err) {
+    console.error("RESEND ERROR:", err);
+    throw err;
+  }
 }
