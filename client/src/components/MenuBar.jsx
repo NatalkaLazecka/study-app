2786aaf238700459a9cc1da85568bd7da3baea23
Zuzanna Kurpik <zuziakurp@gmail.com>
2025-12-31 12:06:02 +0100

    change css MenuBar

--- client/src/components/MenuBar.jsx
+++ client/src/components/MenuBar.jsx
@@ -11,35 +11,35 @@ export default function MenuBar() {
             <div className={styles['menu-icons']}>
 
                 <div className={styles['menu-hover-label']}>
-                    <button className={styles['menu-icon-btn']} onClick={() => navigate('/home')}>
+                    <button className={`${styles['menu-icon-btn']} ${styles['menu-btn-with-label']}`} onClick={() => navigate('/home')}>
                         <i className="fa-solid fa-house"></i>
                          <span className={styles['menu-label']}>Home</span>
                     </button>
                 </div>
 
                 <div className={styles['menu-hover-label']}>
-                    <button className={styles['menu-icon-btn']} onClick={() => navigate('/todo')}>
+                    <button className={`${styles['menu-icon-btn']} ${styles['menu-btn-with-label']}`} onClick={() => navigate('/todo')}>
                         <i className="fa-solid fa-list-check"></i>
                         <span className={styles['menu-label']}>To Do List</span>
                     </button>
                 </div>
 
                 <div className={styles['menu-hover-label']}>
-                    <button className={styles['menu-icon-btn']} onClick={() => navigate('/calendar')}>
+                    <button className={`${styles['menu-icon-btn']} ${styles['menu-btn-with-label']}`} onClick={() => navigate('/calendar')}>
                         <i className="fa-regular fa-calendar"></i>
                         <span className={styles['menu-label']}>Calendar</span>
                     </button>
                 </div>
 
                 <div className={styles['menu-hover-label']}>
-                    <button className={styles['menu-icon-btn']} onClick={() => navigate('/groups')}>
+                    <button className={`${styles['menu-icon-btn']} ${styles['menu-btn-with-label']}`} onClick={() => navigate('/groups')}>
                         <i className="fa-solid fa-people-group"></i>
                         <span className={styles['menu-label']}>Groups</span>
                     </button>
                 </div>
 
                 <div className={styles['menu-hover-label']}>
-                    <button className={styles['menu-icon-btn']} onClick={() => navigate('/schedule')}>
+                    <button className={`${styles['menu-icon-btn']} ${styles['menu-btn-with-label']}`} onClick={() => navigate('/schedule')}>
                         <i className="fa-solid fa-table"></i>
                         <span className={styles['menu-label']}>Schedule</span>
                     </button>
