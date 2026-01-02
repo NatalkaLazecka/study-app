f23252fe9c638e825a9327939bdbb4436df9bbbd
NatalkaLazecka <natalia.lazecka@gmail.com>
2026-01-02 18:57:40 +0100

    debug use httpOnly cookies

--- client/src/features/auth/api/calendarApi.js
+++ client/src/features/auth/api/calendarApi.js
@@ -1,11 +1,11 @@
 import { apiFetch } from "./apiClient";
 
-export async function getStudentEvents(studentId) {
+export async function getStudentEvents() {
   const res = await apiFetch(`/api/events/student`);
   return res.json();
 }
 
-export async function getTodaySchedule(studentId) {
+export async function getTodaySchedule() {
   const res = await apiFetch(`/api/schedule/student/today`);
   return res.json();
 }
