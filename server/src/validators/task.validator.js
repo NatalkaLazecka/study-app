db16cf9f4eb01901aa6207147d988fa136aa1859
NatalkaLazecka <natalia.lazecka@gmail.com>
2026-01-03 12:22:30 +0100

    .

--- server/src/validators/task.validator.js
+++ server/src/validators/task.validator.js
@@ -1,43 +1,44 @@
-import { body, param } from "express-validator";
+import {body, param} from "express-validator";
 
 export const taskIdValidator = [
-  param("id")
-    .isUUID()
-    .withMessage("Invalid task id"),
+    param("id")
+        .isUUID()
+        .withMessage("Invalid task id"),
 ];
 
 export const createTaskValidator = [
-  body("tytul")
-    .trim()
-    .isLength({ min: 3 })
-    .withMessage("Title must have at least 3 characters"),
+    body("tytul")
+        .trim()
+        .isLength({min: 3})
+        .withMessage("Title must have at least 3 characters"),
 
-  body("deadline")
-    .isISO8601()
-    .withMessage("Invalid deadline"),
+    body("deadline")
+        .isISO8601()
+        .withMessage("Invalid deadline"),
 
-  body("priorytet")
-    .optional()
-    .isInt({ min: 1, max: 3 }),
+    body("priorytet")
+        .optional()
+        .isInt({min: 1, max: 3}),
 
-  body("wysilek")
-    .optional()
-    .isInt({ min: 1, max: 4 }),
+    body("wysilek")
+        .optional()
+        .isInt({min: 1, max: 4}),
 
-  body("automatyczne_powiadomienie")
-    .optional()
-    .isIn([0, 1]),
+    body("automatyczne_powiadomienie")
+        .optional()
+        .isIn([0, 1]),
 
-  body("tryby_powiadomien")
-    .optional()
-    .isArray(),
+    body("tryby_powiadomien")
+        .optional()
+        .isArray(),
 
-  body("tryby_powiadomien.*")
-    .optional()
-    .isInt(),
+    body("tryby_powiadomien.*")
+        .optional()
+        .isUUID()
+        .withMessage("Notification mode ID must be UUID"),
 ];
 
 export const updateTaskValidator = [
-  ...taskIdValidator,
-  ...createTaskValidator,
+    ...taskIdValidator,
+    ...createTaskValidator,
 ];
