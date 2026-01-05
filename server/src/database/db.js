6bf9a8e16e1b954ce5f153f394f02238bb3cbea5
NatalkaLazecka <natalia.lazecka@gmail.com>
2026-01-05 13:14:28 +0100

    ssl fix

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -1,9 +1,5 @@
 import mysql from "mysql2";
-import crypto from "crypto";
 import { env } from "../config/env.js";
-
-const EXPECTED_FINGERPRINT =
-    "1A:EC:00:85:06:45:9C:14:AF:15:0C:B0:20:93:17:25:E5:25:61:9B:8C:4C:78:51:F5:EE:36:18:E8:51:B7:C3";
 
 const pool = mysql.createPool({
     host: env.db.host,
@@ -11,24 +7,10 @@ const pool = mysql.createPool({
     user: env.db.user,
     password: env.db.password,
     database: env.db.name,
+
     ssl: {
         rejectUnauthorized: true,
-
-        checkServerIdentity: (host, cert) => {
-            const fingerprint = crypto
-                .createHash("sha256")
-                .update(cert.raw)
-                .digest("hex")
-                .toUpperCase()
-                .match(/.{2}/g)
-                .join(":");
-
-            if (fingerprint !== EXPECTED_FINGERPRINT) {
-                throw new Error(
-                    `TLS fingerprint mismatch: ${fingerprint}`
-                );
-            }
-        },
+        ca: env.DB_SSL_CA,
     },
 
     waitForConnections: true,
