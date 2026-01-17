505d141cde9a009a36872f55f305e25130e1456f
ZUZIAKURPIK <128489454+ZUZIAKURPIK@users.noreply.github.com>
2026-01-17 15:38:43 +0100

    add todo to group v2

--- server/src/routes/tasks.routes.js
+++ server/src/routes/tasks.routes.js
@@ -6,22 +6,27 @@ import {
     deleteTask,
     getTaskById,
     getTasksByStudent,
-    getNotificationModes, getTasksByGroup, addGroupTask, addGroupTask, updateGroupTask, toggleGroupTask, deleteGroupTask
+    getNotificationModes,
+    getTasksByGroup,
+    addGroupTask,
+    updateGroupTask,
+    toggleGroupTask,
+    deleteGroupTask
 } from "../controllers/tasks.controller.js";
-import { requireAuth } from "../middleware/requireAuth.js";
+import {requireAuth} from "../middleware/requireAuth.js";
 import {
-  createTaskValidator,
-  updateTaskValidator,
-  taskIdValidator,
+    createTaskValidator,
+    updateTaskValidator,
+    taskIdValidator,
 } from "../validators/task.validator.js";
 
-import { validate } from "../middleware/validate.js";
+import {validate} from "../middleware/validate.js";
 
 const router = express.Router();
 
 router.get("/notification-modes", requireAuth, getNotificationModes);
-router.get("/student",requireAuth, getTasksByStudent);
-router.get("/",requireAuth, getTasks);
+router.get("/student", requireAuth, getTasksByStudent);
+router.get("/", requireAuth, getTasks);
 router.post("/", requireAuth, createTaskValidator, validate, addTask);
 router.get("/:id", requireAuth, taskIdValidator, validate, getTaskById);
 router.put("/:id", requireAuth, updateTaskValidator, validate, updateTask);
